40f7184f0de5b7eb57bad3f7985fa2a13dca0dd8
Jevon Morris <iheartbenzene@gmail.com>
2019-05-31 09:35:59 -0700

    intialize negative binomial. refining started.

--- just_some_functions.py
+++ just_some_functions.py
@@ -1,11 +1,18 @@
 import numpy as np
 import scipy
+from math import gamma
 
 def factorial(n):
     if n < 0 and n%2 == 0:
-        pass
+        print("Please enter a non-negative integer.")
     elif n < 0 and n%2 != 0:
-        pass
-    elif n == 0:
+        print("Please enter a non-negative integer.")
+    elif n == 0 or n == 1:
         return 1
     return n * factorial(n-1)
+
+def negative_binomial(k, r, p):
+    return (gamma(k + r) / (factorial(k)*gamma(r))) * (p ** r) * ((1-p) ** k)
+
+def probability_of_success():
+    pass
