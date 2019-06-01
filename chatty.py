ac91a2ba65b9f0779b77b28f863dccdadc91b8c6
Jevon Morris <iheartbenzene@gmail.com>
2019-05-31 21:56:14 -0700

    initialize tokens

--- chatty.py
+++ chatty.py
@@ -3,7 +3,27 @@ import tensorflow as tf
 import tflearn as tfl
 import nltk
 import json
-from smart_open import open
+from pickle import dump, load
 
 from nltk.stem.lancaster import LancasterStemmer
+from smart_open import open
+
+
+with open('intents1.json') as first_intent:
+    data = json.load(first_intent)
+
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+for intent in data['intents']:
+    for pattern in intent['pattern']:
+        word = nltk.word_tokenize(pattern)
+        words.extend(word)
+        docs_x.append(word)
+        docs_y.append(intent['tag'])
+
+        if intent['tag'] not in labels:
+            labels.append(intent['tag'])
 
