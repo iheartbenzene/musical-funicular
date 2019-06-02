ae3f4728cb79c753e1616c199c98c94b4d5bb1a5
Jevon Morris <iheartbenzene@gmail.com>
2019-06-01 21:45:08 -0700

    nothing noteworthy

--- facial_recognition.py
+++ facial_recognition.py
@@ -15,8 +15,14 @@ ap.add_argument('-e', '--encodings', required=True, help='path to serialized dat
 ap.add_argument('-d', '--detection-method', type=str, default='cnn', help='facial detection method: hog or cnn')
 args = vars(ap.parse_args())
 
-print('analyzing faces...')
+print('loading facial analysis...')
 image_paths = list(paths.list_images(args['dataset']))
 encodings_accounted = []
 names_accounted = []
 
+for (i, image_path) in enumerate(image_paths):
+    print('loading image analysis {}/{}...'.format(i+1, len(image_paths)))
+    name = image_path.split(os.path.sep)[-2]
+
+    image = cv2.imread(image_path)
+    rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
