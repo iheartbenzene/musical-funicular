95484cf45014f22617c4c56a5a6f7e48569ccc66
Jevon Morris <iheartbenzene@gmail.com>
2019-06-01 21:29:36 -0700

    nothing noteworthy

--- facial_recognition.py
+++ facial_recognition.py
@@ -3,6 +3,15 @@ import os
 import numpy as np
 import face_recognition
 import dlib
+import argparse
+import pickle
 
+from imutils import paths
 from PIL import Image, ImageDraw
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--dataset', required=True, help='path to the input directory of faces and images')
+ap.add_argument('-e', '--encodings', required=True, help='path to serialized database of facial encodings')
+ap.add_argument('-d', '--detection-method', type=str, default='cnn', help='facial detection method: hog or cnn')
+args = vars(ap.parse_args())
 
