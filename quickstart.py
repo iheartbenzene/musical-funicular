40208073ea3284ca539d4eac3d4f445249f7b033
Jevon Morris <iheartbenzene@gmail.com>
2019-06-02 06:27:10 -0700

    webhook stuff

--- quickstart.py
+++ quickstart.py
@@ -10,3 +10,15 @@ def main():
     message_headers = { 'Content-Type': 'application/json; charset = UTF-8'}
 
     http_obj = Http()
+
+    response = http_obj.request(
+        url = url,
+        method = 'POST',
+        headers = message_headers,
+        body = dumps(bot_message)
+    )
+
+    print(response)
+
+if __name__ == '__main__':
+    main()
