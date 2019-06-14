95ee11694848a1c628f208d08d42cf0d76633765
Jevon M <iheartbenzene@gmail.com>
2019-06-13 18:54:30 -0700

    more notes

--- shape_scraper_alpha/shape_scraper_alpha/spiders/dick_tracy.py
+++ shape_scraper_alpha/shape_scraper_alpha/spiders/dick_tracy.py
@@ -1,45 +1,50 @@
 from collections import defaultdict
 
-origin = dict()
-rank = dict()
+# origin = dict()
+# rank = dict()
 
-def set_set(vertex):
-    origin[vertex] = vertex
-    rank[vertex] = 0
+# def set_set(vertex):
+#     origin[vertex] = vertex
+#     rank[vertex] = 0
     
-def locate(vertex):
-    if origin[vertex] != vertex:
-        origin[vertex] = locate(origin[vertex])
-    return origin(vertex)
+# def locate(vertex):
+#     if origin[vertex] != vertex:
+#         origin[vertex] = locate(origin[vertex])
+#     return origin(vertex)
 
-def unite(vertex1, vertex2):
-    root1 = locate(vertex1)
-    root2 = locate(vertex2)
-    if root1 != root2:
-        if rank[root1] > rank[root2]:
-            origin[root2] = root1
-        else:
-            origin[root1] = root2
-        if rank[root1] == rank[root2]:
-            rank[root2] += 1
+# def unite(vertex1, vertex2):
+#     root1 = locate(vertex1)
+#     root2 = locate(vertex2)
+#     if root1 != root2:
+#         if rank[root1] > rank[root2]:
+#             origin[root2] = root1
+#         else:
+#             origin[root1] = root2
+#         if rank[root1] == rank[root2]:
+#             rank[root2] += 1
 
-def kruskal(graph):
-    for vertex in graph['vertex']:
-        set_set(vertex)
-        min_span_tree = set()
-        edges = list(graph['edges'])
-        edge.sort()
-        #add logging for debugging
+# def kruskal(graph):
+#     for vertex in graph['vertex']:
+#         set_set(vertex)
+#         min_span_tree = set()
+#         edges = list(graph['edges'])
+#         edge.sort()
+#         #add logging for debugging
     
-    for edge in edges:
-        weight, vertex_1, vertex_2 = edge
-        if locate(vertex_1) != locate(vertex_2):
-            unite(vertex1, vertex2)
-            min_span_tree.add(edge)
+#     for edge in edges:
+#         weight, vertex_1, vertex_2 = edge
+#         if locate(vertex_1) != locate(vertex_2):
+#             unite(vertex1, vertex2)
+#             min_span_tree.add(edge)
             
-    return sorted(min_span_tree)
+#     return sorted(min_span_tree)
 
-graph = {}
+graph = {'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F'])
+         'F': set(['C', 'E'])}
 
 #Traversal while keeping track of the nodes
 def depth_first_search_1(graph, start):
@@ -52,6 +57,8 @@ def depth_first_search_1(graph, start):
             node_stack.extend(graph[vertex] - visited_node)
         return visited_node
 
+# depth_first_search_1(graph, 'A')
+
 #Succinct traversal?
 #Implying a smaller space complexity?
 def depth_first_search_2(graph, start, visited_node = None):
@@ -62,13 +69,29 @@ def depth_first_search_2(graph, start, visited_node = None):
         depth_first_search_2(graph, next, visited_node)
     return visited_node
 
+# depth_first_search_2(graph, 'C')
 #
 def depth_first_search_paths_1(graph, start, goal):
     node_stack = [(start, [start])]
-    while node_stack
+    while node_stack:
+        (vertex, path) = node_stack.pop()
+        for next in graph[vertex] - set(path):
+            if next == goal:
+                yield path + [next]
+            else:
+                node_stack.append((next, path + [next]))
+                
+# list(depth_first_search_paths_1(graph, 'A', 'F'))
 
 def depth_first_search_paths_2(graph, start, goal, path = None):
-    pass
+    if path is None:
+        path = [start]
+    if start == goal:
+        yield path
+    for next in graph[start] - set(path):
+        yield from depth_first_search_paths_2(graph, next, goal, path + [next])
+        
+# list(depth_first_search_paths_2(graph, 'C', 'F'))
 
 def breadth_first_search_1(graph, start):
     pass
