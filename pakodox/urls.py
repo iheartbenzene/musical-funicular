5d714f3eee3869b322a32a3bbee3eb454bcfb79a
Jevon M <iheartbenzene@gmail.com>
2019-06-18 15:38:32 -0700

    restructure

--- pakodox/urls.py
+++ pakodox/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('hello_worls.urls')),
+    path('', include('hello_world.urls')),
 ]
